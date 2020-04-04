156a2cb7c7e9de45f0e85d94dba67fe9f80c2344
unknown <adriancerejo@gmail.com>
2020-04-04 13:15:38 -0400

    Responsive for mobile devices and tablet devices. Also made slight changes to styling

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
-import React , { useEffect, useState }from 'react';
-import Picture from './Picture';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Picture from "./Picture";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
 
 const App = () => {
-  
-  const client_id=`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`;
+  const client_id = `XXXXXXXXXX`;
 
   const [images, setImages] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('mountains');
-  
-  useEffect(() =>{
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("mountains");
+
+  useEffect(() => {
     getImages();
   }, [query]);
 
-  const updateSearch = e => {
+  const updateSearch = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
-  const getSearch = e => {
+  const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
-    setSearch("")
-  }
+    setSearch("");
+  };
 
   const getImages = async () => {
-    const response = await fetch (
+    const response = await fetch(
       `https://api.unsplash.com/search/photos/?client_id=${client_id}&query=${query}&per_page=30`
     );
     const data = await response.json();
     setImages(data.results);
     console.log(data.results);
-}
+  };
 
- 
   return (
-    <div className="App">  
-      <form onSubmit={getSearch} className="search-form">
-        <TextField className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="Search for Images"/>
-        <Button variant='contained' color='primary' className='search-button' type="submit">Search</Button>
-      </form>
+    <>
+      <header>
+        <form onSubmit={getSearch} className="search-form">
+          <TextField
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+            placeholder="Search for Images"
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Search
+          </Button>
+        </form>
+      </header>
 
-          {images.map(image => (
-            <Picture key={image.id} source={image.links.download} firstName={image.user.first_name} lastName={image.user.last_name} likes={image.likes} thumbnail={image.urls.thumb}/>
-          ) 
-          )}    
-    </div>
+      <div className="App">
+        {images.map((image) => (
+          <Picture
+            key={image.id}
+            source={image.links.download}
+            firstName={image.user.first_name}
+            lastName={image.user.last_name}
+            likes={image.likes}
+            thumbnail={image.urls.thumb}
+          />
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/Picture.js
+++ src/Picture.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import {FaHeart} from 'react-icons/fa';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-const Picture = ({source, firstName, lastName, likes, thumbnail}) => {
-    console.log(thumbnail)
-    return (
-        <div className="tile">
-            <p>Uploaded By: {firstName} {lastName}</p>
-            <p>
-            <FaHeart/> {likes} 
-            </p>
-            {/* <img className="picture" src={source} alt='' /> */}
-            <LazyLoadImage className="picture"
-            alt=""
-            effect="blur"
-            placeholderSrc={thumbnail}
-            src={source}
-            />
+const Picture = ({ source, firstName, lastName, likes, thumbnail }) => {
+  console.log(thumbnail);
+  return (
+    <div className="tile">
+      <div className="info">
+        <div className="name">
+          Uploaded By: {firstName} {lastName}
         </div>
-    );
-}
+        <div className="likes">
+          <FaHeart /> {likes}
+        </div>
+      </div>
+      {/* <img className="picture" src={source} alt='' /> */}
+      <LazyLoadImage
+        className="picture"
+        alt=""
+        effect="blur"
+        placeholderSrc={thumbnail}
+        src={source}
+      />
+    </div>
+  );
+};
 
-export default Picture
+export default Picture;
